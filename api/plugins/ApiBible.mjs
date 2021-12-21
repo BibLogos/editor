102938334fe1df5ea7ddcfc36241ff042f1af844
Daniel Beeke <mail@danielbeeke.nl>
2021-12-21 22:37:40 +0100

    Adding 404

--- api/plugins/ApiBible.mjs
+++ api/plugins/ApiBible.mjs
@@ -3,16 +3,9 @@ export default {
         const parsedURL = new URL(request.url)
         const apiUrl = `https://api.scripture.api.bible/v1/${pathParts.join('/')}${parsedURL.search}`
         const proxyRequest = new Request(apiUrl, { headers: { 'api-key': env.API_BIBLE_KEY } })
-        const cache = caches.default
-
-        let response = await cache.match(proxyRequest)
-
-        if (!response) {
-            response = await fetch(proxyRequest)
-            response.headers.set("Access-Control-Allow-Origin", request.headers.get('origin'))
-            response.headers.set('Cache-Control', 'max-age=360000')
-            ctx.waitUntil(cache.put(proxyRequest, response.clone()))
-        }
+        response = await fetch(proxyRequest)
+        response.headers.set("Access-Control-Allow-Origin", request.headers.get('origin'))
+        response.headers.set('Cache-Control', 'max-age=360000')
 
         return response
     }   
