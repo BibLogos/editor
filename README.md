1de0f307aca2235985994445334efe5d0e7d3ad1
Daniel Beeke <mail@danielbeeke.nl>
2022-01-05 13:06:17 +0100

    build for 0.0.1

--- README.md
+++ README.md
@@ -4,21 +4,20 @@
 - In which cities has Jesus been with his disciples? 
 - Who has seen Paul?
 
-This editor helps with creating a dataset that may present those facts. It is mainly based on human work. A person can login and click on words or sentences in the bible text and link that to a corresponding description of it. For example you can click on "Alternative name" or "Person".
+This editor helps with creating a dataset that may present those facts. It is mainly based on human work. A person can click on words or sentences in the bible text and link that to a corresponding description of it. For example you can click on "Alternative name" or "Person". After that the person can save it or send it in for review.
 
 # Technical stack:
 
 - SPARQL
 - Turtle
-- Apache Jena
+- GitHub API for saving data and projects
 - TypeScript
 - https://github.com/WebReflection/ube
 - universal-router
 - Snowpack
-- Docker Compose for launching Apache Jena
 - https://scripture.api.bible/
 
-## A small demonstration of the product progress (links to YouTube)
+## A small demonstration of the product progress (links to YouTube, old version without Github integration)
 
 [<img src="https://i.ytimg.com/vi_webp/XYsRmsiwMNY/maxresdefault.webp">](https://www.youtube.com/watch?v=XYsRmsiwMNY)
 
---
+++ publish.js
@@ -0,0 +1,63 @@
+import { config } from 'dotenv'
+import fetch from 'node-fetch'
+import { exec } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
+import readline from 'readline'
+
+config()
+
+console.clear()
+
+const npmPackageText = readFileSync('package.json')
+const npmPackage = JSON.parse(npmPackageText)
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`Specify version: (${npmPackage.version})`, async function (version) {
+  if (!version) {
+    version = npmPackage.version
+  }
+  
+  if (version !== npmPackage.version) {
+    npmPackage.version = version
+    writeFileSync('package.json', JSON.stringify(npmPackage))
+  }
+
+  rl.close();
+
+  try {
+    await run('git add -A')
+    await run(`git commit -m "build for ${version}"`)
+    await run(`git push`) 
+  }
+  catch (exception) {
+    console.log(exception)
+  }
+})
+
+const run = async (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      
+      if (error) {
+          reject(`error: ${error.message}`);
+          return;
+      }
+      
+      if (stderr) {
+          reject(`stderr: ${stderr}`);
+          return;
+      }
+
+      resolve(stdout);
+    });  
+  })
+}
+
+// await run('git add docs')
+// await run(`git commit -m "new build"`)
+// await run(`git push`)
+// git add docs && git commit -m \"new build\" && git push && cd api && wrangler publish
